acac6aeb7522aeb1645af0673c3cd8aad0dddf93
Meer Hyder Siddiqui <153274363+hydxr@users.noreply.github.com>
2025-11-09 20:26:59 +0530

    Refactor index.js for improved readability

--- index.js
+++ index.js
@@ -3,31 +3,28 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 const tabBtn = document.getElementById("tab-btn")
 
-// Get leads from localStorage
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
-// Save current tab
-tabBtn.addEventListener("click", function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
         myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
         render(myLeads)
     })
 })
 
-// Render leads list
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target="_blank" href="${leads[i]}">
+                <a target='_blank' href='${leads[i]}'>
                     ${leads[i]}
                 </a>
             </li>
@@ -36,19 +33,15 @@ function render(leads) {
     ulEl.innerHTML = listItems
 }
 
-// Delete all leads on double-click
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
     render(myLeads)
 })
 
-// Save input manually
 inputBtn.addEventListener("click", function() {
-    if (inputEl.value.trim() !== "") {
-        myLeads.push(inputEl.value)
-        inputEl.value = ""
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        render(myLeads)
-    }
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
 })
